71a146524a76c6f2c9513fd669713601373e329a
T-REX911 <163975890+T-REX911@users.noreply.github.com>
2025-05-25 20:41:49 +0530

    Security Config

---
+++ SpringSecurity_02/src/main/java/com/spring/SpringSecurity_02/SpringSecurity_02/Config/SecurityConfig.java
@@ -0,0 +1,47 @@
+package com.spring.SpringSecurity_02.SpringSecurity_02.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(csrf -> csrf.disable()) // Disable CSRF for APIs
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/product/create").hasAnyRole("ADMIN", "MANAGER")
+                        .requestMatchers("/product/update").hasAnyRole("ADMIN", "MANAGER", "CASHIER")
+                        .requestMatchers("/product/delete").hasRole("ADMIN")
+                        .requestMatchers("/product/read").hasAnyRole("ADMIN", "MANAGER", "CASHIER", "USER")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults()); // Use basic auth
+        return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("admin").password("admin123").roles("ADMIN").build(),
+                User.withUsername("manager").password("manager123").roles("MANAGER").build(),
+                User.withUsername("cashier").password("cashier123").roles("CASHIER").build(),
+                User.withUsername("user").password("user123").roles("USER").build()
+        );
+    }
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public static org.springframework.security.crypto.password.PasswordEncoder passwordEncoder() {
+        // No password encryption, for simplicity
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
--- SpringSecurity_02/src/main/java/com/spring/SpringSecurity_02/SpringSecurity_02/SpringSecurity02Application.java
+++ SpringSecurity_02/src/main/java/com/spring/SpringSecurity_02/SpringSecurity_02/SpringSecurity02Application.java
@@ -2,9 +2,8 @@ package com.spring.SpringSecurity_02.SpringSecurity_02;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
+@SpringBootApplication//(exclude = {SecurityAutoConfiguration.class})
 public class SpringSecurity02Application {
 
 	public static void main(String[] args) {
